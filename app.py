e2467390c58e21eac38baadb613d9415f80883e3
Lalit Punjabi <lalitpunjabi5@gmail.com>
2026-09-09 15:49:23 +0530

    test changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD Pipeline!"
+    return "Hello from Jenkins CI/CD Pipeline - Version 2!"
 
 @app.route("/health")
 def health():
